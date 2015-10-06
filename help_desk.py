13581cf7fa5fde06fa061ddc6aab510efc78f0ce
stoneleaf <devnull@localhost>
2015-10-05 20:16:30 -0700

    fix typo

--- help_desk.py
+++ help_desk.py
@@ -80,7 +80,7 @@ class help_desk(osv.Model):
         if not assigned:
             values['message_notify_ids'] = self._get_partner_ids(cr, 'Triage')
         else:
-            values['message_follower_user_ids'].append[assigned]
+            values['message_follower_ids'].append(assigned)
             user = self.pool.get('res.users').browse(cr, SUPERUSER_ID, assigned, context=context)
             if user.has_group('fnx_hd.fnx_help_desk_evs'):
                 values['state'] = 'evs'
